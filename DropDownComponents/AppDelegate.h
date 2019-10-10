ce493875c2de1c8d2a208f7a16be279466646cef
settybo <2368251556@qq.com>
2019-10-10 16:16:20 +0800

    初始化项目

---
+++ DropDownComponents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DropDownComponents
+//
+//  Created by ZHANG QINGBO on 2019/10/10.
+//  Copyright © 2019 ZHANG QINGBO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DropDownComponents/New Group/SGOrefresh.h
@@ -0,0 +1,21 @@
+//
+//  SGOrefresh.h
+//  DropDownComponents
+//
+//  Created by ZHANG QINGBO on 2019/10/10.
+//  Copyright © 2019 ZHANG QINGBO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SGOrefresh : UIScrollView
+
+/**
+ 是否缓慢拖拽
+ */
+@property (nonatomic,assign) BOOL isDragScrollow;
+@end
+
+NS_ASSUME_NONNULL_END
